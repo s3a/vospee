21cfd1a6dfb291e5f574487a9ddc0dcd55c60673
s3a <s3a@users.noreply.github.com>
2025-11-02 22:51:51 +0300

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// Этот файл будет автоматически сгенерирован во время деплоя
-// Не редактируйте его вручную!
-window.CONFIG = {
-    webhookUrl: '%%WEBHOOK_URL%%',
-    bytescaleApiKey: '%%BYTESCALE_API_KEY%%',
-    bytescaleUploadUrl: '%%BYTESCALE_UPLOAD_URL%%'
-};
